f76e79d90354b67abc0bf92db4796916c8115129
monji05 <monji4869@gmail.com>
2022-11-05 21:47:48 +0900

    p33

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,6 @@
 package lexer
 
-import "monkey/token"
+import "./monkey/token/toke"
 
 type Lexer struct {
   input string
@@ -52,7 +52,7 @@ func (l *Lexer) NextToken() token.Token {
       tok = token.Token{Type: token.NOT_EQ, Literal: literal}
     } else {
       tok = newToken(token.BANG, l.ch)
-    } 
+    }
   case '/':
     tok = newToken(token.SLASH, l.ch)
   case '*':
---
+++ parser/parser_test.go
@@ -0,0 +1,14 @@
+package parser
+
+import (
+  "testing"
+  "monkey/ast"
+  "monkey/lexer"
+)
+
+func TestLetStatement(t *testing.T) {
+  input := `
+let x = 5;
+let y = 10;
+  `
+}
